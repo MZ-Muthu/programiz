19d2d572691026f62f10ee0b4b603d4bc075d1ea
MZ_Muthu <muthazhagan187@gmail.com>
2021-01-10 09:32:32 +0530

    First commit

---
+++ 1.js
@@ -0,0 +1,18 @@
+function name(a){
+if(a % 2 != 0)
+{
+    console.log("Weird");
+}
+else if(a % 2 == 0 && (a >= 2 && a <= 5))
+{
+    console.log("Not Weird");
+}
+else if(a % 2 == 0 && (a >= 6 && a <= 20))
+{
+    console.log("Weird");
+}
+else 
+{
+    console.log("Weird");
+}
+}
